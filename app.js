58138749f179179b55e0bea5f74c58550269f5d2
beratyukselen <beratyukselen@gmail.com>
2025-12-27 02:03:11 +0300

    feat: Connect to MySQL database and render homepage with dynamic posts

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const path = require('path');
-const { time } = require('console');
-const { title } = require('process');
-
 dotenv.config();
+console.log("KONTROL EDİYORUM -> Kullanıcı:", process.env.DB_USER);
+const path = require('path');
+const db = require('./config/db');
+
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('index' , {
-        title: 'SSR Blog Projesi',
-        message: 'Merahaba Dünya! Bu benim ilk SSR sayfam!'
-    });
+app.get('/', async (req, res) => {
+    try {
+        const [posts] = await db.execute('SELECT * FROM posts ORDER BY created_at DESC');
+
+        res.render('index', {
+            title: 'Ana Sayfa | SSR Blog',
+            posts: posts
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Veritabanı hatası oluştu!");
+    }
 });
 
 const PORT = process.env.PORT || 3000;
